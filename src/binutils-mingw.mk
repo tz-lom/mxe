e371b0579cf93949dce44431cf2984f39becbe11
Nuzhdin Yury <nuzhin.urii@gmail.com>
2015-08-14 09:19:53 +0300

    All except GCC is good

---
+++ src/binutils-mingw.mk
@@ -0,0 +1,37 @@
+# This file is part of MXE.
+# See index.html for further information.
+
+PKG             := binutils-mingw
+$(PKG)_IGNORE   :=
+$(PKG)_VERSION  := 2.25.1
+$(PKG)_CHECKSUM := 1d597ae063e3947a5f61e23ceda8aebf78405fcd
+$(PKG)_SUBDIR   := binutils-$($(PKG)_VERSION)
+$(PKG)_FILE     := binutils-$($(PKG)_VERSION).tar.bz2
+$(PKG)_URL      := http://ftp.gnu.org/pub/gnu/binutils/$($(PKG)_FILE)
+$(PKG)_URL_2    := ftp://ftp.cs.tu-berlin.de/pub/gnu/binutils/$($(PKG)_FILE)
+$(PKG)_DEPS     := gcc 
+
+define $(PKG)_UPDATE
+    $(WGET) -q -O- 'http://ftp.gnu.org/gnu/binutils/?C=M;O=D' | \
+    $(SED) -n 's,.*<a href="binutils-\([0-9][^"]*\)\.tar.*,\1,p' | \
+    $(SORT) -V | \
+    tail -1
+endef
+
+define $(PKG)_BUILD
+    cd '$(1)' && ./configure \
+        $(MXE_CONFIGURE_OPTS) \
+        --disable-multilib \
+        --with-gcc \
+        --with-gnu-ld \
+        --with-gnu-as \
+        --disable-nls \
+        --disable-shared \
+        --disable-werror
+    $(MAKE) -C '$(1)' -j '$(JOBS)'
+    $(MAKE) -C '$(1)' -j 1 install
+
+    rm -f $(addprefix $(PREFIX)/$(TARGET)/bin/, ar as dlltool ld ld.bfd nm objcopy objdump ranlib strip)
+endef
+
+# $(PKG)_BUILD_$(BUILD) :=
---
+++ src/gcc-mpc-mingw.mk
@@ -0,0 +1,30 @@
+# This file is part of MXE.
+# See index.html for further information.
+
+PKG             := gcc-mpc-mingw
+$(PKG)_IGNORE    = $(mpc_IGNORE)
+$(PKG)_VERSION   = $(mpc_VERSION)
+$(PKG)_CHECKSUM  = $(mpc_CHECKSUM)
+$(PKG)_SUBDIR    = $(mpc_SUBDIR)
+$(PKG)_FILE      = $(mpc_FILE)
+$(PKG)_URL       = $(mpc_URL)
+$(PKG)_URL_2     = $(mpc_URL_2)
+$(PKG)_DEPS     := gcc-gmp-mingw gcc-mpfr-mingw
+
+define $(PKG)_UPDATE
+    echo $(mpc_VERSION)
+endef
+
+define $(PKG)_BUILD
+    mkdir '$(1).build'
+    cd    '$(1).build' && '$(1)/configure' \
+        --host='$(TARGET)' \
+        --build='$(BUILD)' \
+        --prefix='$(PREFIX)/$(TARGET)' \
+        --disable-shared \
+        --with-gmp='$(PREFIX)/$(TARGET)'
+    $(MAKE) -C '$(1).build' -j '$(JOBS)'
+    $(MAKE) -C '$(1).build' -j 1 install
+endef
+
+$(PKG)_BUILD_$(BUILD) =
